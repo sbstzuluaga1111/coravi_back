1f786be010277e8e50fe84c1ce23ba1e2a65e64d
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2024-02-02 14:47:20 -0500

    v3

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
-
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
@@ -18,15 +17,65 @@ app.post('/send-email', async (req, res) => {
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,  
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     const mailOptions = {
       from: `"Nombre del remitente" <remitente@example.com>`,
       to: 'sbstzuluaga1111@gmail.com',
-      subject: 'Nuevo formulario de sugerencias',
-      html: `<p>Nombre: ${nombre}</p><p>Correo: ${correo}</p><p>Comentario: ${comentario}</p><p>Autorización: ${autorizacion ? 'Sí' : 'No'}</p>`,
+      subject: 'Comentario',
+      html: `
+        <p>Nombre: ${nombre}</p>
+        <p>Correo: ${correo}</p>
+        <p>Comentario: ${comentario}</p>
+        <p>Autorización de comunicaciones: ${autorizacion ? 'Sí' : 'No'}</p>
+      `,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log('Correo enviado con éxito. URL del mensaje de prueba:', nodemailer.getTestMessageUrl(info));
+
+    res.status(200).send('Correo enviado con éxito');
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+    res.status(500).send(error.toString());
+  }
+});
+
+
+app.post('/send-email-form2', async (req, res) => {
+  const { nombre, telefono, correo, comentario, autorizacion, fecha } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    // Formatear la fecha
+    const formattedFecha = new Date(fecha).toLocaleDateString('es-ES', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    const mailOptions = {
+      from: `"Nombre del remitente" <remitente@example.com>`,
+      to: 'sbstzuluaga1111@gmail.com',
+      subject: 'Agendamiento de cita',
+      html: `
+        <p>Nombre: ${nombre}</p>
+        <p>Teléfono: ${telefono}</p>
+        <p>Correo: ${correo}</p>
+        <p>Comentario: ${comentario}</p>
+        <p>Autorización de comunicaciones: ${autorizacion ? 'Sí' : 'No'}</p>
+        <p>Fecha de la cita: ${formattedFecha}</p>
+      `,
     };
 
     const info = await transporter.sendMail(mailOptions);
